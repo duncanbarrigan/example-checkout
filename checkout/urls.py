62cc3bfa4101309e53c1e1fc9d002e023880557e
Duncan Barrigan <duncanbarrigan@gmail.com>
2017-01-01 20:09:50 +0000

    Reroute /

--- checkout/urls.py
+++ checkout/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'$', include('example_checkout.urls')),
     url(r'^example_checkout/', include('example_checkout.urls')),
     url(r'^admin/', admin.site.urls),
 ]
